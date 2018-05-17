6acb4386049b728d3bf9ce8a4734e1cc5d89af22
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-17 17:16:10 -0300

    First Screen 70% Set

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Header, Button } from 'react-native-elements';
+import { 
+  Text,
+  View,
+  Image
+} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Header 
+          centerComponent={{ text: 'CMM', style: { color: '#fff' } }}
+        />
+        <View style={styles.container}> 
+          <Text style={styles.textStyle}>Cloud Mining</Text>
+          <Text style={styles.textStyle2}>Manager</Text>
+          <Image
+            style={{width: 250, height: 250, paddingTop: 35}}
+            source={{uri: 'http://www.pngpix.com/wp-content/uploads/2016/10/PNGPIX-COM-Bitcoin-PNG-Image-500x496.png'}}
+        />
+          <Button
+            title='Contratos'
+          />
+        </View>
+        
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 35,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  textStyle: {
+    fontSize: 30,
+  },
+  textStyle2: {
+    fontSize: 42,
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+}
