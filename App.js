73a2d2ce4e38c472305cb56b320c0059bea1463a
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-18 14:18:13 -0300

    Added Imports for testing Screens

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import FirstScreen from './src/components/FirstScreen';
 import SettingsScreen from './src/components/SettingsScreen';
 import ContratosScreen from './src/components/ContratosScreen';
+import AdicionarContratosScreen from './src/components/AdicionarContratosScreen';
+import ContratosForm from './src/components/ContratosForm';
+import EditarContratosScreen from './src/components/EditarContratosScreen';
 
 export default class App extends React.Component {
   render() {
     return (
-      <FirstScreen />
+      <EditarContratosScreen />
     );
   }
 }
