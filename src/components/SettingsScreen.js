2e4ef5fd2e2cae6bab5257c3b3973060ca1b4e4d
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-18 11:36:03 -0300

    Created SettingsScreen.js

---
+++ src/components/SettingsScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    Header,
+    Button
+} from 'react-native-elements';
+import {
+    Text,
+    View,
+    Image
+} from 'react-native';
+
+const SettingsScreen = () => (
+    <View>
+        <Header
+            backgroundColor={'#F8F8FF'} // Color: GhostWhite
+            centerComponent={{ text: 'SETTINGS', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
+            leftComponent={{ icon: 'left-arrow', color: 'rgba(92, 99,216, 1)' }}
+        />
+    </View>
+);
+
+export default SettingsScreen;
