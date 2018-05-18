20cce53fd1aa061e100d920c2633a9e0ea70050b
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-18 11:44:54 -0300

    Changed arrow icon on header

--- src/components/SettingsScreen.js
+++ src/components/SettingsScreen.js
@@ -14,7 +14,7 @@ const SettingsScreen = () => (
         <Header
             backgroundColor={'#F8F8FF'} // Color: GhostWhite
             centerComponent={{ text: 'SETTINGS', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
-            leftComponent={{ icon: 'left-arrow', color: 'rgba(92, 99,216, 1)' }}
+            leftComponent={{ icon: 'keyboard-arrow-left', color: 'rgba(92, 99,216, 1)' }}
         />
     </View>
 );
