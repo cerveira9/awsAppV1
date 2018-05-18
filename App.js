25387da7e2f3fa2a27dbe14fbdc8a3f25768dce7
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-18 11:44:41 -0300

    Added import for settings screen test

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FirstScreen from './src/components/FirstScreen';
+import SettingsScreen from './src/components/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
