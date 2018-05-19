e2217252ee884e7531f12b04c6c88ede99034c8f
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-19 13:54:55 -0300

    Removed left arrow from header

--- src/components/SettingsScreen.js
+++ src/components/SettingsScreen.js
@@ -21,7 +21,6 @@ class SettingsScreen extends Component {
                 <Header
                     backgroundColor={'#F8F8FF'} // Color: GhostWhite
                     centerComponent={{ text: 'SETTINGS', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
-                    leftComponent={{ icon: 'keyboard-arrow-left', color: 'rgba(92, 99,216, 1)' }}
                 />
             </View >
         );
