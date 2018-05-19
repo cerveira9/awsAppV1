095f72a45f621b4e10bfae5e695a5122b0b5452c
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-19 11:33:44 -0300

    Added Background Image

--- src/components/EditarContratosScreen.js
+++ src/components/EditarContratosScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
     View,
-    Dimensions
+    Dimensions,
+    Image
 } from 'react-native';
 import {
     Header,
@@ -9,7 +10,9 @@ import {
 } from 'react-native-elements';
 import ContratosForm from './ContratosForm';
 
+const remote = 'http://colorhunt.co/blog/gradient-wallpapers-mobile/colorhunt-5.png';
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class EditarContratosScreen extends Component {
     render() {
@@ -20,39 +23,52 @@ class EditarContratosScreen extends Component {
                     centerComponent={{ text: 'EDITAR', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
                     leftComponent={{ icon: 'keyboard-arrow-left', color: 'rgba(92, 99,216, 1)' }}
                 />
-                <View style={styles.contratosFormStyle}>
-                    <ContratosForm />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="SALVAR MUDANÇAS"
-                        titleStyle={{ fontWeight: 'bold' }}
-                        buttonStyle={{
-                            backgroundColor: "rgba(92, 99,216, 1)",
-                            width: 300,
-                            height: 45,
-                            borderColor: "transparent",
-                            borderWidth: 0,
-                            borderRadius: 30
+                <View>
+                    <Image
+                        style={{
+                            backgroundColor: 'rgba(92, 99,216, 1)',
+                            flex: 1,
+                            position: 'absolute',
+                            width: SCREEN_WIDTH,
+                            height: SCREEN_HEIGHT,
+                            justifyContent: 'center',
                         }}
-                        containerStyle={{ marginTop: 20 }}
+                        source={{ uri: remote }}
                     />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="DELETAR"
-                        titleStyle={{ fontWeight: 'bold' }}
-                        buttonStyle={{
-                            backgroundColor: "rgba(92, 99,216, 1)",
-                            width: 300,
-                            height: 45,
-                            borderColor: "transparent",
-                            borderWidth: 0,
-                            borderRadius: 30
-                        }}
-                        containerStyle={{ marginTop: 20 }}
-                    />
-                </View>
+                    <View style={styles.contratosFormStyle}>
+                        <ContratosForm />
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title="SALVAR MUDANÇAS"Í
+                            titleStyle={{ fontWeight: 'bold' }}
+                            buttonStyle={{
+                                backgroundColor: "rgba(92, 99,216, 1)",
+                                width: 300,
+                                height: 45,
+                                borderColor: "transparent",
+                                borderWidth: 0,
+                                borderRadius: 30
+                            }}
+                            containerStyle={{ marginTop: 20 }}
+                        />
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title="DELETAR"
+                            titleStyle={{ fontWeight: 'bold' }}
+                            buttonStyle={{
+                                backgroundColor: "rgba(92, 99,216, 1)",
+                                width: 300,
+                                height: 45,
+                                borderColor: "transparent",
+                                borderWidth: 0,
+                                borderRadius: 30
+                            }}
+                            containerStyle={{ marginTop: 20 }}
+                        />
+                    </View>
+                </View>Í
             </View>
         );
     }
