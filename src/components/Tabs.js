2c91ecf25809e548d3c6437d3802b89a53b6821e
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-20 15:07:44 -0300

    Changed Layout

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -8,79 +8,81 @@ import {
 
 class Tabs extends Component {
 
-    // Initialize State
-    state = {
-      // First tab is active by default
-      activeTab: 0
-    }
-  
-    // Pull children out of props passed from App component
-    render({ children } = this.props) {
-      return (
-        <View style={styles.container}>
-          {/* Tabs row */}
-          <View style={styles.tabsContainer}>
-            {/* Pull props out of children, and pull title out of props */}
-            {children.map(({ props: { title } }, index) =>
-              <TouchableOpacity
-                style={[
-                  // Default style for every tab
-                  styles.tabContainer,
-                  // Merge default style with styles.tabContainerActive for active tab
-                  index === this.state.activeTab ? styles.tabContainerActive : []
-                ]}
-                // Change active tab
-                onPress={() => this.setState({ activeTab: index }) }
-                // Required key prop for components generated returned by map iterator
-                key={index}
-              >
-                <Text style={styles.tabText}>
-                  {title}
-                </Text>
-              </TouchableOpacity>
-            )}
-          </View>
-          {/* Content */}
-          <View style={styles.contentContainer}>
-            {children[this.state.activeTab]}
-          </View>
-        </View>
-      );
-    }
+  // Initialize State
+  state = {
+    // First tab is active by default
+    activeTab: 0
   }
 
-  const styles = StyleSheet.create({
-    // Component container
-    container: {
-      flex: 1,                            // Take up all available space
-    },
-    // Tabs row container
-    tabsContainer: {
-      flexDirection: 'row',               // Arrange tabs in a row
-      paddingTop: 30,                     // Top padding
-    },
-    // Individual tab container
-    tabContainer: {
-      flex: 1,                            // Take up equal amount of space for each tab
-      paddingVertical: 15,                // Vertical padding
-      borderBottomWidth: 3,               // Add thick border at the bottom
-      borderBottomColor: 'transparent',   // Transparent border for inactive tabs
-    },
-    // Active tab container
-    tabContainerActive: {
-      borderBottomColor: '#FFFFFF',       // White bottom border for active tabs
-    },
-    // Tab text
-    tabText: {
-      color: '#FFFFFF',
-      fontFamily: 'Avenir',
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-    // Content container
-    contentContainer: {
-      flex: 1                             // Take up all available space
-    }
-  });
+  // Pull children out of props passed from App component
+  render({ children } = this.props) {
+    return (
+      <View style={styles.container}>
+        {/* Tabs row */}
+        <View style={styles.contentContainer}>
+          {children[this.state.activeTab]}
+        </View>
+        <View style={styles.tabsContainer}>
+          {/* Pull props out of children, and pull title out of props */}
+            {children.map(({ props: { title } }, index) =>
+            <TouchableOpacity
+              style={[
+                // Default style for every tab
+                styles.tabContainer,
+                // Merge default style with styles.tabContainerActive for active tab
+                index === this.state.activeTab ? styles.tabContainerActive : []
+              ]}
+              // Change active tab
+              onPress={() => this.setState({ activeTab: index })}
+              // Required key prop for components generated returned by map iterator
+              key={index}
+            >
+              <Text style={styles.tabText}>
+                {title}
+              </Text>
+            </TouchableOpacity>
+          )}
+        </View>
+        {/* Content */}
 
-  export default Tabs;
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // Component container
+  container: {
+    flex: 1,                            // Take up all available space
+  },
+  // Tabs row container
+  tabsContainer: {
+    flexDirection: 'row',               // Arrange tabs in a row
+    paddingTop: 5,                     // Top padding
+    backgroundColor: 'rgba(92, 99,216, 1)', 
+  },
+  // Individual tab container
+  tabContainer: {
+    flex: 1,                            // Take up equal amount of space for each tab
+    paddingVertical: 5,                // Vertical padding
+    borderBottomWidth: 3,               // Add thick border at the bottom
+    borderBottomColor: 'transparent',   // Transparent border for inactive tabs
+  },
+  // Active tab container
+  tabContainerActive: {
+    borderBottomColor: '#FFFFFF',       // White bottom border for active tabs
+  },
+  // Tab text
+  tabText: {
+    color: '#FFFFFF',
+    fontFamily: 'Avenir',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  // Content container
+  contentContainer: {
+    flex: 1                             // Take up all available space
+  }
+});
+
+export default Tabs;
