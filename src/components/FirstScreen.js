3796207bc160e7dece325d785e2da8ebb7bcf09d
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-20 12:03:17 -0300

    changed styling

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -85,7 +85,7 @@ class FirstScreen extends Component {
 
 const styles = {
     container: {
-        marginTop: 60,
+        marginTop: 10,
         paddingTop: 25,
         alignItems: 'center',
         justifyContent: 'center',
