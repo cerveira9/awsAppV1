6407978a5ecf213fc7cf410adeb5670a0f325db5
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-20 15:50:18 -0300

    Changed Styling and delete unused tabnav code

--- src/components/SettingsScreen.js
+++ src/components/SettingsScreen.js
@@ -16,30 +16,10 @@ const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class SettingsScreen extends Component {
 
-    // static navigationOptions = ({navigation}) => ({
-    //     tabBarLabel:"Settings",
-    //     tabBarIcon:({tintColor, focused}) => {
-    //         if(focused){
-    //           return(
-    //             <Image source={require('../img/icons/settings_on.png')} style={{width:20, height:20, tintColor: 'rgba(92, 99,216, 1)'}} />
-    //           );
-    //         }else{
-    //           return(
-    //             <Image source={require('../img/icons/settings_off.png')} style={{width:20, height:20, tintColor: 'rgba(92, 99,216, 1)'}} />
-    //           );
-    //         }
-            
-    //     }
-    // });
-
     render() {
         return (
-            <View style={{ paddingTop: 15 }}>
+            <View style={{ paddingTop: 30 }}>
             <Text>Teste</Text>
-                {/*<Header
-                    backgroundColor={'#F8F8FF'} // Color: GhostWhite
-                    centerComponent={{ text: 'SETTINGS', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
-                />*/}
             </View >
         );
     }
