1e486e26af834e04a924ed94bbd6995ace3064fc
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-19 22:41:04 -0300

    Added a marginTop

--- src/components/ContratosScreen.js
+++ src/components/ContratosScreen.js
@@ -37,7 +37,7 @@ class ContratosScreen extends Component {
 
     render() {
         return (
-            <View>
+            <View style={{ marginTop: 15 }}>
                 {/*<Header
                     backgroundColor={'#F8F8FF'} // Color: GhostWhite
                     centerComponent={{ text: 'CONTRATOS', style: { color: 'rgba(92, 99,216, 1)', fontSize: 20, fontWeight: 'bold' } }}
