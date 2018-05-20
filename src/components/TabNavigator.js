5c0e28d5789ccf2a66865be5e00db5e8fa1054f9
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-20 15:07:30 -0300

    SAved some code if needed in the future

--- src/components/TabNavigator.js
+++ src/components/TabNavigator.js
@@ -30,3 +30,19 @@ const TabNav = TabNavigator({
 export default TabNav;
 
 console.disableYellowBox = true;
+
+   // static navigationOptions = ({navigation}) => ({
+    //     tabBarLabel:"Contratos",
+    //     tabBarIcon:({tintColor, focused}) => {
+    //         if(focused){
+    //           return(
+    //             <Image source={require('../img/icons/contracts_on.png')} style={{width:20, height:20, tintColor: 'rgba(92, 99,216, 1)'}} />
+    //           );
+    //         }else{
+    //           return(
+    //             <Image source={require('../img/icons/contracts_off.png')} style={{width:20, height:20, tintColor: 'rgba(92, 99,216, 1)'}} />
+    //           );
+    //         }
+            
+    //     }
+    // });
