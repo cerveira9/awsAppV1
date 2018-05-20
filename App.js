1026cb598c0dadf45e3cb376ef2ba458a460cc3f
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2018-05-20 12:02:49 -0300

    Implemented Universal tab

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TabBarIOS, Component } from 'react-native';
+import Tabs from './src/components/Tabs';
 import FirstScreen from './src/components/FirstScreen';
 import SettingsScreen from './src/components/SettingsScreen';
 import ContratosScreen from './src/components/ContratosScreen';
@@ -9,51 +10,81 @@ var contractsIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAA
 var settingsIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAABb0lEQVR4AeTUAUdDURjG8b+yjdEAFMAATbZY9REKrCkBQvsQUWH7HhWbGQmIoBVM1J0WWA011h2VpgDLpLnBdey+zsndFdDvhcv77LHdY4f/Kk6egjt54gS2jzMyRQK79BRdEdibp6hHQFM4YmL4NEefc7JMAPNYsogGKWCSVar0SWFUcz/QpcwQRzNDynTd5xoGazhjzjoaEeyxi2xCSOwawg8cUODUsN3RH7WcNmlcpLnRJHpIbDEQoSemGRXmXiQG5NBI0PbEVpAWxfdNYBClqWIddBpq3yQKZtcqeIRORe0tfmWpYAWdY39FUe5U8BYdW+1bRDFIipe9gLTs52Xn+BKH2yLMqBls5PFvgvSOg5wGaXAt0dEkXkHaw9HOCQUOaRm223/zp30khMbG2EUZDOrqYisZLrZvijy7z1WMUnxyRsa9auvIGoskMEmWC/rM4lMMWRQhoA9PzQtBiR9XI7CSp+hns6jXiBiZAADLu77Go53e3QAAAABJRU5ErkJggg==';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'FirstScreen'
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     selectedTab: 'FirstScreen'
+  //   };
+  // }
 
   render() {
     return (
-      <TabBarIOS selectedTab={this.state.selectedTab}>
-        <TabBarIOS.Item
-          title="Home"
-          icon={{uri: homeIcon}}
-          selected={this.state.selectedTab === 'FirstScreen'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'FirstScreen'
-            });
-          }}>
-          <FirstScreen />
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title="Contratos"
-          icon={{uri: contractsIcon}}
-          selected={this.state.selectedTab === 'ContratosScreen'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'ContratosScreen'
-            });
-          }}>
-          <ContratosScreen />
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title="Settings"
-          icon={{uri: settingsIcon}}
-          selected={this.state.selectedTab === 'SettingsScreen'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'SettingsScreen'
-            });
-          }}>
-          <SettingsScreen />
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <View style={styles.container}>
+        <Tabs>
+          {/* First tab */}
+          <View title="HOME" style={styles.content}>
+            <FirstScreen />
+          </View>
+          {/* Second tab */}
+          <View title="CONTRATOS" style={styles.content}>
+            <ContratosScreen />
+          </View>
+          {/* Third tab */}
+          <View title="SETTINGS" style={styles.content}>
+            <SettingsScreen />
+          </View>
+        </Tabs>
+      </View>
+      // <TabBarIOS selectedTab={this.state.selectedTab}>
+      //   <TabBarIOS.Item
+      //     title="Home"
+      //     icon={{uri: homeIcon}}
+      //     selected={this.state.selectedTab === 'FirstScreen'}
+      //     onPress={() => {
+      //       this.setState({
+      //         selectedTab: 'FirstScreen'
+      //       });
+      //     }}>
+      //     <FirstScreen />
+      //   </TabBarIOS.Item>
+      //   <TabBarIOS.Item
+      //     title="Contratos"
+      //     icon={{uri: contractsIcon}}
+      //     selected={this.state.selectedTab === 'ContratosScreen'}
+      //     onPress={() => {
+      //       this.setState({
+      //         selectedTab: 'ContratosScreen'
+      //       });
+      //     }}>
+      //     <ContratosScreen />
+      //   </TabBarIOS.Item>
+      //   <TabBarIOS.Item
+      //     title="Settings"
+      //     icon={{uri: settingsIcon}}
+      //     selected={this.state.selectedTab === 'SettingsScreen'}
+      //     onPress={() => {
+      //       this.setState({
+      //         selectedTab: 'SettingsScreen'
+      //       });
+      //     }}>
+      //     <SettingsScreen />
+      //   </TabBarIOS.Item>
+      // </TabBarIOS>
     );
+  }
+}
+
+const styles = {
+  iconStyle: {
+    color: 'rgba(92, 99,216, 1)'
+  },
+  container: {
+    flex: 1,                            // Take up all screen
+    backgroundColor: 'rgba(92, 99,216, 1)',         // Background color
+  },
+  content: {
+    backgroundColor: 'white'
+    
   }
 }
 
